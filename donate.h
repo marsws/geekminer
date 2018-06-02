e8daa6dd89983a01fc1036338fb2acd40c9b4d87
quasars84 <cgcoins@gmail.com>
2018-06-02 21:54:40 +0100

    initial commit

---
+++ donate.h
@@ -0,0 +1,9 @@
+#ifndef DONATE_H
+#define DONATE_H
+
+#define MIN_DEV_DONATE_PERCENT 1
+
+// 100 minutes
+#define DONATE_CYCLE_TIME 6000
+
+#endif
---
+++ x13/cuda_x13.h
@@ -0,0 +1,4 @@
+extern void x13_hamsi512_cpu_hash_64(int thr_id, uint32_t threads, uint32_t *d_hash);
+extern void x13_fugue512_cpu_hash_64(int thr_id, uint32_t threads, uint32_t *d_hash);
+extern void x14_shabal512_cpu_hash_64(int thr_id, uint32_t threads, uint32_t *d_hash);
+
